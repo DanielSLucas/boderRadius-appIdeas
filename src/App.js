78a617c8cd62595933187a92f4dfd17f42eb2fbc
DanielSLucas <daniellucas-pms@hotmail.com>
2020-04-04 12:32:57 -0300

    not solved

--- src/App.js
+++ src/App.js
@@ -1,26 +1,118 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './styles.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      topLeft: 0, 
+      topRight: 0, 
+      botLeft: 0, 
+      botRight: 0,
+      altered: false,
+    }
+
+    this.handleInputChange  = this.handleInputChange.bind(this);
+    this.handleSubmit       = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    // const {topLeft} = this.state;
+    // const {topRight} = this.state;
+    // const {botLeft} = this.state;
+    // const {botRight} = this.state;
+
+    this.setState({
+      altered: true,
+    })
+  }
+
+  render () {
+    return(
+      <div className="main-container">
+         
+        <h1>Border-radius previewer</h1>
+
+        <main>
+          <form onSubmit={this.handleSubmit}>
+            <input
+              type="number"
+              name="topLeft" 
+              value={this.state.topLeft} 
+              onChange={this.handleInputChange}
+            />
+
+            <input
+              type="number"
+              name="topRight" 
+              value={this.state.topRight} 
+              onChange={this.handleInputChange}
+            />
+
+            <input
+              type="number"
+              name="botLeft" 
+              value={this.state.botLeft} 
+              onChange={this.handleInputChange}
+            />
+
+            <input
+              type="number"
+              name="botRight" 
+              value={this.state.botRight} 
+              onChange={this.handleInputChange}
+            />
+
+            <button type="submit">Aplicar</button>
+
+          </form>
+
+          <hr/>
+
+          {this.state.altered ? (
+            <div  
+              className="theDiv"  
+              style={{
+                borderTopLeftRadius: this.state.topLeft,
+                borderTopRightRadius: this.state.topRight,
+                borderBottomLeftRadius: this.state.botLeft,
+                borderBottomRightRadius: this.state.botRight,
+              }} 
+            >
+            </div>
+          ): (
+            <div  
+              className="theDiv"  
+              style={{
+                borderTopLeftRadius: this.state.topLeft,
+                borderTopRightRadius: this.state.topRight,
+                borderBottomLeftRadius: this.state.botLeft,
+                borderBottomRightRadius: this.state.botRight,
+              }} 
+            >
+            </div>
+          ) }
+
+          
+        </main>
+
+      </div>
+    );
+  }
+    
 }
 
 export default App;

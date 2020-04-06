0ff117134e25194f36e0f7ea41f2190680ce9658
DanielSLucas <daniellucas-pms@hotmail.com>
2020-04-06 11:07:01 -0300

    Functional

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './styles.css';
 
 class App extends React.Component {
@@ -10,11 +10,11 @@ class App extends React.Component {
       topRight: 0, 
       botLeft: 0, 
       botRight: 0,
-      altered: false,
     }
 
     this.handleInputChange  = this.handleInputChange.bind(this);
     this.handleSubmit       = this.handleSubmit.bind(this);
+    this.handleClick        = this.handleClick.bind(this);
   }
 
   handleInputChange(event) {
@@ -30,14 +30,29 @@ class App extends React.Component {
   handleSubmit(e) {
     e.preventDefault();
 
-    // const {topLeft} = this.state;
-    // const {topRight} = this.state;
-    // const {botLeft} = this.state;
-    // const {botRight} = this.state;
+    const {topLeft} = this.state;
+    const {topRight} = this.state;
+    const {botLeft} = this.state;
+    const {botRight} = this.state;
 
-    this.setState({
-      altered: true,
-    })
+    document.getElementById('theDiv').style.borderTopLeftRadius = topLeft.toString() + "px";
+    document.getElementById('theDiv').style.borderTopRightRadius = topRight.toString() + "px";
+    document.getElementById('theDiv').style.borderBottomLeftRadius = botLeft.toString() + "px";
+    document.getElementById('theDiv').style.borderBottomRightRadius = botRight.toString() + "px";
+ 
+  }
+
+  handleClick() {
+    var css = document.getElementById("theCSS");
+
+    css.select();
+    css.setSelectionRange(0, 999999)
+    document.execCommand("copy");
+    // alert("Copied the text: " + css.value);
+
+
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Copied: " + css.value;
   }
 
   render () {
@@ -47,6 +62,20 @@ class App extends React.Component {
         <h1>Border-radius previewer</h1>
 
         <main>
+
+          <div className="css-container"> 
+            <input 
+              id="theCSS"
+              value={this.state.topLeft + " " + this.state.topRight + " " + this.state.botLeft + " " + this.state.botRight} 
+            /> 
+              
+            <button onClick={this.handleClick} type="button" >
+              <span className="tooltiptext" id="myTooltip">Copy to clipboard</span>
+              Copiar              
+            </button>
+          </div>
+
+
           <form onSubmit={this.handleSubmit}>
             <input
               type="number"
@@ -82,29 +111,18 @@ class App extends React.Component {
 
           <hr/>
 
-          {this.state.altered ? (
-            <div  
-              className="theDiv"  
-              style={{
-                borderTopLeftRadius: this.state.topLeft,
-                borderTopRightRadius: this.state.topRight,
-                borderBottomLeftRadius: this.state.botLeft,
-                borderBottomRightRadius: this.state.botRight,
-              }} 
-            >
-            </div>
-          ): (
-            <div  
-              className="theDiv"  
-              style={{
-                borderTopLeftRadius: this.state.topLeft,
-                borderTopRightRadius: this.state.topRight,
-                borderBottomLeftRadius: this.state.botLeft,
-                borderBottomRightRadius: this.state.botRight,
-              }} 
-            >
-            </div>
-          ) }
+          
+          <div  
+            id="theDiv"  
+            // style={{
+            //   borderTopLeftRadius: this.state.topLeft,
+            //   borderTopRightRadius: this.state.topRight,
+            //   borderBottomLeftRadius: this.state.botLeft,
+            //   borderBottomRightRadius: this.state.botRight,
+            // }} 
+          >
+          </div>
+          
 
           
         </main>
